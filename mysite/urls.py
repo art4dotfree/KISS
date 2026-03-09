4c01e0a46f67197cf4f933ea9a71ee3b1aa59689
Art4an <160108858+art4dotfree@users.noreply.github.com>
2026-03-09 21:07:54 +0300

    KISS

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,10 @@
 # filename urls
 # author hardh
 # datetime 27.03.2017 - 0:23
-from django.conf.urls import url
+from django.urls import path
 
 from mysite import views
 
-urlpatterns=[
-    url(r'^$',views.post_list, name='post_list')
+urlpatterns = [
+    path('', views.post_list, name='post_list')
 ]
